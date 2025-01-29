da298e1564e1c186a3b9ac13304602b4f1d837d4
Devin Alexander <devinmalexander@gmail.com>
2025-01-29 06:06:31 -0600

    Signals + Instancing Scenes in Code

--- Effects/Effect.gd
+++ Effects/Effect.gd
@@ -1,9 +1,7 @@
 extends AnimatedSprite2D
 
-#func _ready():
-	## warning-ignore: return_value_discarded
-	#connection("animation_finished", self "_on_animation-finished")
-	#play("Animate")
-	#
-#func _on_animation_finished():
-	#queue_free()
+func _ready():
+	play("Animate")
+	
+func _on_animation_finished():
+	queue_free()
--- World/grass.gd
+++ World/grass.gd
@@ -2,11 +2,22 @@ extends Node2D
 
 const GrassEffect = preload("res://Effects/GrassEffect.tscn")
 
-func create_grass_effect():
-	var grassEffect = GrassEffect.instance()
-	get_parent().add_child(grassEffect)
-	grassEffect.global_position = global_position
-	
-func _on_Hurtbox_area_entered(_area):
-	create_grass_effect()
-	queue_free()
+func _process(delta):
+	if Input.is_action_just_pressed("attack"):
+		#var grassEffect = GrassEffect.instance()
+		#var world = get_tree().current_scene
+		#world.add_child(grassEffect)
+		#grassEffect.global_position = global_position
+		var grassEffect = load("res://Effects/GrassEffect.tscn").instantiate()
+		grassEffect.global_position = global_position
+		get_tree().current_scene.add_child(grassEffect)
+		queue_free()
+
+#func create_grass_effect():
+	#var grassEffect = GrassEffect.instance()
+	#get_parent().add_child(grassEffect)
+	#grassEffect.global_position = global_position
+	#
+#func _on_hurtbox_area_entered(area: Area2D) -> void:
+	#create_grass_effect()
+	#queue_free()
