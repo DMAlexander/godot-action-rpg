8cecb7b2c69aae7dbf239c918282bedde587ba76
Devin Alexander <devinmalexander@gmail.com>
2025-01-29 07:01:49 -0600

    Got collisions working

--- World/grass.gd
+++ World/grass.gd
@@ -1,17 +1,11 @@
 extends Node2D
 
 const GrassEffect = preload("res://Effects/GrassEffect.tscn")
-
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		#var grassEffect = GrassEffect.instance()
-		#var world = get_tree().current_scene
-		#world.add_child(grassEffect)
-		#grassEffect.global_position = global_position
-		var grassEffect = load("res://Effects/GrassEffect.tscn").instantiate()
-		grassEffect.global_position = global_position
-		get_tree().current_scene.add_child(grassEffect)
-		queue_free()
+		
+func create_grass_effect():
+	var grassEffect = load("res://Effects/GrassEffect.tscn").instantiate()
+	grassEffect.global_position = global_position
+	get_tree().current_scene.add_child(grassEffect)
 
 #func create_grass_effect():
 	#var grassEffect = GrassEffect.instance()
@@ -21,3 +15,8 @@ func _process(delta):
 #func _on_hurtbox_area_entered(area: Area2D) -> void:
 	#create_grass_effect()
 	#queue_free()
+
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	create_grass_effect()
+	queue_free()
